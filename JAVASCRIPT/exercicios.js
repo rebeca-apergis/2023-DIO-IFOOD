8cd055fb6f6f7c4ad21bc5cde51e8517836c0d52
rebeca-apergis <rebecaapergis@gmail.com>
2024-01-05 19:58:48 -0300

    commit aula

--- JAVASCRIPT/exercicios.js
+++ JAVASCRIPT/exercicios.js
@@ -1,4 +1,4 @@
-const precoBase = 100;
+/*const precoBase = 100;
 let pixDin15 = precoBase - (precoBase * 0.15);
 let deb10 = precoBase - (precoBase * 0.10);
 let credParce2 = precoBase;
@@ -21,5 +21,25 @@ if (pagamento === 'Débito') {
 } else {
     console.log('Selecione uma forma de pagamento valida.');
 }
+*/
 
+function precoFinal(formaPagamento,precoBase){
+    if (formaPagamento === 1 ) { precoBase = precoBase - (precoBase * 0.10); }
+    if (formaPagamento === 2 || formaPagamento === 3) { precoBase = precoBase - (precoBase * 0.15); }
+    if (formaPagamento === 4 ) { precoBase = precoBase; }
+    if (formaPagamento === 5 ) { precoBase = precoBase + (precoBase * 0.10); }
+    return precoBase;
+    
+}
+
+function pagamento(forma){
+    if (forma = 1) { forma = 'Débito'}
+    if ((forma = 2) || (forma = 3)) { forma = 'Dinheiro ou Pix.'}
+    if (forma = 4) { forma = 'Crédito em 2x.'}
+    if (forma = 1) { forma = 'Crédito em várias vezes.'}
+    return forma;
+}
+
+console.log('O preço final ficou em: R$' + precoFinal(5,100).toFixed(2));
+console.log('E o seu pagamento foi feito em ' + pagamento(5));
 
