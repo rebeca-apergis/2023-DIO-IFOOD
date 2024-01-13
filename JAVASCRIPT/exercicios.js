d6d63e5a75b9137adf0a046fa5b68cd914a91b9e
rebeca-apergis <rebecaapergis@gmail.com>
2024-01-13 19:54:39 -0300

    1468485

--- JAVASCRIPT/exercicios.js
+++ JAVASCRIPT/exercicios.js
@@ -14,7 +14,7 @@ acuçar sintatico >:D
 
 =========================================*/
 
-const {gets,print} = require('./funcoes_auxiliares.js');
+//const {gets,print} = require('./funcoes_auxiliares.js');
 
 /*const numerosSorteados = [];
 
@@ -42,6 +42,43 @@ for (let i = 0; i < quantidadeDeAlunos; i++) {
     
 }
 
-print(maiorValorEncontrado);*/
+print(maiorValorEncontrado);
+=================================*/
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
 
-
+// Definição da classe ItemMagico
+class ItemMagico {
+    tipo;
+    dano;
+    resistencia;
+    //TODO: Crie adequadamente um construtor que receba todos os atributos referente ao item mágico:
+   constructor(tipo, dano, resistencia) {
+    this.tipo = tipo;
+    this.dano = dano;
+    this.resistencia = resistencia;
+  }
+  
+    calcularDano() {
+      return this.tipo === 'arma' ? this.dano * 2 : this.dano;
+    }
+  }
+  
+  // Solicita ao usuário para digitar o tipo do item mágico, o dano no item e a resistência do item
+  const tipoItem = 'arma';
+  const danoItem = 300;
+  const resistenciaItem = 300;
+  
+  //TODO: Crie o de um objeto ItemMagico personalizado com base no tipo escolhido
+  
+  const itemPersonalizado = new ItemMagico(tipoItem,danoItem,resistenciaItem)
+  
+  // TODO: Imprima os atributos do item personalizado
+  console.log("Tipo: " + itemPersonalizado.tipo);
+  console.log("Dano: " + itemPersonalizado.dano);
+  console.log("Resistencia: " + itemPersonalizado.resistencia);
+  
+  // Calcula e imprime o dano causado pelo item personalizado em um combate simulado
+  const danoTotal = itemPersonalizado.calcularDano();
+  console.log("Dano em combate: " + danoTotal);
